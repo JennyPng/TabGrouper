166413dd9b5768f985e3ea1ff4f495ca04d93e93
Jenny Peng <jennypscale@gmail.com>
2022-12-29 14:11:49 -0800

    start UI

--- popup.js
+++ popup.js
@@ -1,27 +1,63 @@
+const gButton = document.getElementById("group_button");
+const template = document.getElementById("li_template");
+const elements = new Set();
+
+// TODO: define array to store current groups in storage / load from storage?
+
 async function groupTabs() {
-    let queryOptions = {currentWindow: true, active: true};
+    let queryOptions = { currentWindow: true, active: true };
     const currTab = await chrome.tabs.query(queryOptions);
     const tabs = await chrome.tabs.query({});
 
     const tempurl = currTab[0].url;
     if (!tabs || !tempurl) return; // if either is undefined just don't proceed
-    
+
     const currURL = new URL(tempurl);
     const currHost = currURL.hostname;
 
-    // get tabs with matching host to current tab
+    // get tabs that have the same host as current tab
     const matches = tabs.filter(tab => {
         let url = new URL(tab.url);
         if (url.hostname === currHost) {
-            return url.hostname;
+            return tab;
         }
     });
 
     // group tabs
     const tabIds = matches.map(({ id }) => id);
     const group = await chrome.tabs.group({ tabIds });
-    await chrome.tabGroups.update(group, { title: `${currHost}`});
-  }
+    await chrome.tabGroups.update(group, { title: `${currHost}` });
 
-const gButton = document.getElementById("group-button");
+
+    // UI update
+    for (const tab of matches) {
+        console.log(tab);
+        const element = template.content.firstElementChild.cloneNode(true); // template allows it to be cloned instead of creating new element every time? - for building stuff up in javascript rather than hardcoding html
+
+        const title = tab.title;
+        const pathname = new URL(tab.url).pathname.slice("\docs".length);
+        const favicon = tab.favIconUrl;
+
+        if (favicon) {
+            element.querySelector("img").src = favicon;
+        }
+        element.querySelector(".title").textContent = title;
+        element.querySelector(".pathname").textContent = pathname;
+        element.querySelector("a").addEventListener("click", async () => {
+            // focus window and active tab
+            await chrome.tabs.update(tab.id, { active: true });
+            await chrome.windows.update(tab.windowId, { focused: true });
+        });
+        console.log(tab.title);
+        elements.add(element);
+    }
+    document.querySelector("ul").append(...elements);
+
+    // TODO: update storage to persist data
+    chrome.storage.local.set({ key: value }).then(() => {
+        console.log("Value is set to " + value);
+    });
+
+}
+
 gButton.addEventListener('click', groupTabs);
